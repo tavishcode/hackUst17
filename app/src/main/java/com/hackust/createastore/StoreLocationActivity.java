246148cc57003f597d3eaf1d1860cf6bc020a3e8
tavishcode <tavish.gobindram@gmail.com>
2017-04-22 18:30:49 +0800

    added layouts

---
+++ app/src/main/java/com/hackust/createastore/StoreLocationActivity.java
@@ -0,0 +1,16 @@
+package com.hackust.createastore;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Tavish on 22-Apr-17.
+ */
+
+public class StoreLocationActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
